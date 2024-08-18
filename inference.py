bc63272d3289212c2cbc04029848b35813abed4d
IronMan <sockwz@163.com>
2024-08-18 17:44:49 +0800

    将inference的avatar修改为新的avatar类

--- inference.py
+++ inference.py
@@ -3,8 +3,9 @@ import argparse
 import torch
 from omegaconf import OmegaConf
 
-from musetalk.avatar import Avatar
-from musetalk.utils.utils import load_all_model
+# from musetalk.avatar import Avatar
+# from musetalk.utils.utils import load_all_model
+from musetalk_plus.avatar import Avatar
 
 AVATAR_DIR = "./results/avatars"
 
@@ -45,36 +46,39 @@ if __name__ == "__main__":
         default="musetalk"
     )
     args = parser.parse_args()
-    whisper, vae, unet, pe = load_all_model(args.afe)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    timesteps = torch.tensor([0], device=device)
-    pe = pe.half()
-    vae.vae = vae.vae.half()
-    unet.model = unet.model.half()
+    # whisper, vae, unet, pe = load_all_model(args.afe)
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # timesteps = torch.tensor([0], device=device)
+    # pe = pe.half()
+    # vae.vae = vae.vae.half()
+    # unet.model = unet.model.half()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     inference_config = OmegaConf.load(args.inference_config)
 
     for avatar_id in inference_config:
         data_preparation = inference_config[avatar_id]["preparation"]
         video_path = inference_config[avatar_id]["video_path"]
         bbox_shift = inference_config[avatar_id]["bbox_shift"]
-        avatar = Avatar(
-            unet,
-            vae,
-            pe,
-            whisper,
-            avatar_id=avatar_id,
-            video_path=video_path,
-            bbox_shift=bbox_shift,
-            batch_size=args.batch_size,
-            preparation=data_preparation
-        )
+        avatar = Avatar(str(avatar_id), video_path, bbox_shift, device)
+        # avatar = Avatar(
+        #     unet,
+        #     vae,
+        #     pe,
+        #     whisper,
+        #     avatar_id=avatar_id,
+        #     video_path=video_path,
+        #     bbox_shift=bbox_shift,
+        #     batch_size=args.batch_size,
+        #     preparation=data_preparation
+        # )
         audio_clips = inference_config[avatar_id]["audio_clips"]
         for audio_num, audio_path in audio_clips.items():
             print("Inferring using:", audio_path)
-            avatar.inference(
-                audio_path,
-                args.text,
-                audio_num,
-                args.fps,
-                args.realtime
-            )
+            avatar.inference(audio_path)
+            # avatar.inference(
+            #     audio_path,
+            #     args.text,
+            #     audio_num,
+            #     args.fps,
+            #     args.realtime
+            # )
