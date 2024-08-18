c917bed67be556c378701e4839e7d3ce31632cc5
IronMan <sockwz@163.com>
2024-08-18 22:16:54 +0800

    修复bug

--- musetalk_plus/models.py
+++ musetalk_plus/models.py
@@ -5,7 +5,11 @@ from diffusers import UNet2DConditionModel
 class MuseTalkModel(nn.Module):
     def __init__(self, model_path):
         super().__init__()
-        self.unet = UNet2DConditionModel.from_pretrained(model_path, use_safetensors=False)
+        self.unet = UNet2DConditionModel.from_pretrained(
+            model_path, use_safetensors=False,
+            low_cpu_mem_usage=False,  # 不使用低内存模式
+            device_map=None  # 不使用设备映射
+        )
 
     def forward(self, inputs):
         image_feature, audio_feature = inputs
