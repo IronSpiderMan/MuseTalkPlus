d3b2bbf273bdad954f9c45b3c13e51071f11ae63
IronMan <sockwz@163.com>
2024-08-19 09:28:45 +0800

    完善了准备数据集的脚步，允许准备数据集时使用固定的人脸位置

--- musetalk_plus/train/prepare_dataset.py
+++ musetalk_plus/train/prepare_dataset.py
@@ -1,14 +1,12 @@
-import os
 import sys
-import glob
 import shutil
 import argparse
 from pathlib import Path
 
 import cv2
-import tqdm
 import torch
 import numpy as np
+from tqdm import tqdm
 
 sys.path.append('.')
 
@@ -26,15 +24,12 @@ afe = AudioFrameExtractor(r"F:\models\whisper-tiny-zh")
 fr = FaceRecognizer()
 
 
-def process_video(video_path="./data/video/zack.mp4", fixed_face=True):
+def process_video(video_path, fixed_face=True):
     face_location = None
-    video_name = os.path.basename(video_path).split('.')[0]
+    video_name = video_path.stem
     recreate_multiple_dirs([
-        VIDEO_FRAME_DIR / video_name,
-        VIDEO_LATENT_DIR / video_name,
-        AUDIO_FEATURE_DIR / video_name,
-        TMP_FRAME_DIR,
-        TMP_AUDIO_DIR
+        VIDEO_FRAME_DIR / video_name, VIDEO_LATENT_DIR / video_name,
+        AUDIO_FEATURE_DIR / video_name, TMP_FRAME_DIR, TMP_AUDIO_DIR
     ])
     # 提取视频帧
     video2images(video_path, TMP_FRAME_DIR)
@@ -42,52 +37,58 @@ def process_video(video_path="./data/video/zack.mp4", fixed_face=True):
     audio_path = video2audio(video_path, TMP_AUDIO_DIR)
     # 提取特征
     feature_chunks = afe.extract_frames(audio_path)
-    # 截取脸部
-    path_pattern = TMP_FRAME_DIR / video_name / "*"
-    img_list = list(glob.glob(str(path_pattern)))
-    if face_location is None:
-        y1, x2, y2, x1 = fr.face_locations(img_list[0])
-        face_location = [x1, y1, x2, y2]
-    coord_list = [[*face_location] for _ in range(len(img_list))]
+    img_list = list((TMP_DATASET_DIR / video_name).glob('*'))
+    # 截取脸部，如果设置了fixed_face，则只使用第一帧的面部位置截取
+    if fixed_face:
+        if face_location is None:
+            y1, x2, y2, x1 = fr.face_locations(img_list[0])
+            face_location = [x1, y1, x2, y2]
+        coord_list = [[*face_location] for _ in range(len(img_list))]
+    else:
+        coord_list = []
+        for img in img_list:
+            y1, x2, y2, x1 = fr.face_locations(str(img))
+            coord_list.append([x1, y1, x2, y2])
     frame_list = read_images(img_list)
-    for idx, (coord, frame, chunk) in enumerate(zip(coord_list, frame_list, feature_chunks)):
-        try:
-            x1, y1, x2, y2 = coord
-            crop_frame = frame[y1:y2, x1:x2]
-            resized_crop_frame = cv2.resize(crop_frame, (256, 256), interpolation=cv2.INTER_LANCZOS4)
-            dst = VIDEO_FRAME_DIR / video_name / f"{idx}.png"
-            cv2.imwrite(str(dst), resized_crop_frame)
-            dst = AUDIO_FEATURE_DIR / video_name / f"{idx}.npy"
-            np.save(str(dst), chunk)
-        except Exception as e:
-            print(e)
+    for idx, (coord, frame, chunk) in tqdm(
+            enumerate(zip(coord_list, frame_list, feature_chunks)),
+            total=len(frame_list),
+            desc=f"Processing video: {video_name}"
+    ):
+        x1, y1, x2, y2 = coord
+        crop_frame = frame[y1:y2, x1:x2]
+        resized_crop_frame = cv2.resize(crop_frame, (256, 256), interpolation=cv2.INTER_LANCZOS4)
+        dst = VIDEO_FRAME_DIR / video_name / f"{idx:08d}.png"
+        cv2.imwrite(str(dst), resized_crop_frame)
+        dst = AUDIO_FEATURE_DIR / video_name / f"{idx:08d}.npy"
+        np.save(str(dst), chunk)
     shutil.rmtree(TMP_DATASET_DIR)
 
 
-def process_videos(video_dir="./datasets/videos", include_latents=False):
-    # video_list = Path(video_dir).glob("/*.mp4")
-    for file in tqdm.tqdm(glob.glob(video_dir + "/*.mp4")):
-        process_video(file, include_latents)
+def process_videos(video_dir="./datasets/videos", fixed_face=True):
+    video_list = list(Path(video_dir).glob("*.mp4"))
+    for video_path in tqdm(video_list, total=len(video_list), desc='Processing videos'):
+        process_video(video_path, fixed_face)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--include_latents",
-        type=bool,
-        default=False
-    )
-    parser.add_argument(
         "--videos_dir",
         type=str,
         default="./datasets/videos"
+    )
+    parser.add_argument(
+        "--fixed_face",
+        type=bool,
+        default=True
     )
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    process_videos(video_dir=args.videos_dir, include_latents=args.include_latents)
+    process_videos(video_dir=args.videos_dir, fixed_face=args.fixed_face)
 
 
 if __name__ == '__main__':
