87cbfaeaecde6474e6311175331cd4f69bc30284
IronMan <sockwz@163.com>
2024-08-26 17:14:03 +0800

    优化inference

--- inference.py
+++ inference.py
@@ -3,8 +3,6 @@ import argparse
 import torch
 from omegaconf import OmegaConf
 
-# from musetalk.avatar import Avatar
-# from musetalk.utils.utils import load_all_model
 from musetalk_plus.avatar import Avatar
 
 AVATAR_DIR = "./results/avatars"
@@ -46,12 +44,6 @@ if __name__ == "__main__":
         default="musetalk"
     )
     args = parser.parse_args()
-    # whisper, vae, unet, pe = load_all_model(args.afe)
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # timesteps = torch.tensor([0], device=device)
-    # pe = pe.half()
-    # vae.vae = vae.vae.half()
-    # unet.model = unet.model.half()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     inference_config = OmegaConf.load(args.inference_config)
 
@@ -60,25 +52,7 @@ if __name__ == "__main__":
         video_path = inference_config[avatar_id]["video_path"]
         bbox_shift = inference_config[avatar_id]["bbox_shift"]
         avatar = Avatar(str(avatar_id), video_path, bbox_shift, device)
-        # avatar = Avatar(
-        #     unet,
-        #     vae,
-        #     pe,
-        #     whisper,
-        #     avatar_id=avatar_id,
-        #     video_path=video_path,
-        #     bbox_shift=bbox_shift,
-        #     batch_size=args.batch_size,
-        #     preparation=data_preparation
-        # )
         audio_clips = inference_config[avatar_id]["audio_clips"]
         for audio_num, audio_path in audio_clips.items():
             print("Inferring using:", audio_path)
             avatar.inference(audio_path)
-            # avatar.inference(
-            #     audio_path,
-            #     args.text,
-            #     audio_num,
-            #     args.fps,
-            #     args.realtime
-            # )
